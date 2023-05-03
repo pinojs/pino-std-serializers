3feb29b68a30d314acbbf1f8810af1cddea147d4
Alex / KATT <alexander@n1s.se>
2023-05-03 16:32:55 +0200

    Add info about `cause` explicitly always being `undefined` (#124)
    * Add info about `cause` explicitly always being `undefined`
    
    * Update index.d.ts
    
    Co-authored-by: Pelle Wessman <pelle@kodfabrik.se>
    
    * Update index.d.ts
    
    ---------
    
    Co-authored-by: Pelle Wessman <pelle@kodfabrik.se>

--- index.d.ts
+++ index.d.ts
@@ -25,6 +25,10 @@ export interface SerializedError {
    */
   raw: Error;
   /**
+   * `cause` is never included in the log output, if you need the `cause`, use {@link raw.cause}
+   */
+  cause?: never;
+  /**
    * Any other extra properties that have been attached to the object will also be present on the serialized object.
    */
   [key: string]: any;
